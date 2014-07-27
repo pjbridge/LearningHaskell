ad757bf7d5cdd788b1e40a46be2e5d52e8e21d22
peerjakob <peer.bridge@enoro.com>
2014-07-27 18:37:31 +0200

    basic functions and types

--- function.hs
+++ function.hs
@@ -1,5 +1,14 @@
+doubleMe :: Int->Int
 doubleMe x=x+x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 
 						then x
 						else x*2
+triangels=[(a,b,c)|c <-[1..10],b <-[1..10],a <- [1..10]]			
+rightTriangels=[(a,b,c)|c <-[1..10],b <-[1..10],a <- [1..10],a^2+b^2==c^2,a+b+c==24]	--right triangels with sides 1..10, perimeter=24	
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+	
+circumference :: Float -> Float
+circumference r = 2*pi*r
