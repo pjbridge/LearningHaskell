1216c67b740ca015e942988509581f91e5d5b2c8
peerjakob <peer.bridge@enoro.com>
2014-07-29 12:37:26 +0200

    Some more funcs

--- function.hs
+++ function.hs
@@ -23,11 +23,29 @@ lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"  
  
  
-factorialB :: Integer -> Integer  --recursively implemented
-factorialB 0 = 1  
-factorialB n = n * factorial (n - 1)  
+factorial' :: Integer -> Integer  --recursively implemented
+factorial' 0 = 1  
+factorial' n = n * factorial (n - 1)  
 
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)  
 
+head' :: [a] -> a  
+head' [] = error "Empty list"  
+head' (x:_) = x  
 
+length' :: [a] -> Integer
+length' [] = 0 
+length' (_:xs) = 1 + length' xs --length is tail +1, recurse
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0 
+sum' (x:xs) = x + sum' xs --sum is  head (first element) plus sum tail (remaining elements)
+
+bmiTell :: (RealFloat a) => a -> a -> String 
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  ++ show bmi
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise                 = "You're a whale, congratulations!" 
+    where bmi=weight / height ^ 2 
